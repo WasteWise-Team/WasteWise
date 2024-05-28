62e53255c24dff010993de95be399178099ee402
Terence Chen <68515161+TCHEN621130@users.noreply.github.com>
2024-05-28 15:42:43 -0700

    home updated

--- src/components/pie-chart.js
+++ src/components/pie-chart.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react'
-import { StyleSheet, ScrollView, Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import PieChart from 'react-native-pie-chart'
 
 export default class TestChart extends Component {
   render() {
     const widthAndHeight = 250
-    const series = [123, 321, 123, 789, 537]
-    const sliceColor = ['#fbd203', '#ffb300', '#ff9100', '#ff6c00', '#ff3c00']
+    const series = [123, 321, 123,]
+    const sliceColor = ['#99DAB3', '#2D5A3D', '#FFFFFF',]
 
     return (
-      <ScrollView style={{ flex: 1 }}>
-        <View style={styles.container}>
-          <Text style={styles.title}>Basic</Text>
-          <PieChart widthAndHeight={widthAndHeight} series={series} sliceColor={sliceColor} />
-          <Text style={styles.title}>Doughnut</Text>
-          <PieChart
-            widthAndHeight={widthAndHeight}
-            series={series}
-            sliceColor={sliceColor}
-            coverRadius={0.45}
-            coverFill={'#FFF'}
-          />
-        </View>
-      </ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.title}>You've recycled:</Text>
+        <PieChart
+          widthAndHeight={widthAndHeight}
+          series={series}
+          sliceColor={sliceColor}
+          coverRadius={0.6}
+          coverFill={'#C4D8BF'}
+        />
+      </View>
     )
   }
 }
@@ -32,8 +28,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
   },
+
   title: {
-    fontSize: 24,
+    fontSize: 20,
     margin: 10,
+    color: '#000000',
+    paddingTop: 20,
   },
 })
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import TestChart from '../components/pie-chart';
 // import * as Font from 'expo-font';
 
 // Get screen dimensions
 const { width } = Dimensions.get('window');
 
 const baseFontSize = width > 600 ? 24 : 16; // Example breakpoint at 600
+
+const Square = () => {
+    return <View style={styles.square} />;
+};
+const Square1 = () => {
+    return <View style={styles.square1} />;
+};
+const Square2 = () => {
+    return <View style={styles.square1} />;
+};
   
 export default function HomeScreen({ navigation }) {
     const [fontsLoaded, setFontsLoaded] = useState(false);
@@ -23,6 +34,7 @@ export default function HomeScreen({ navigation }) {
     if (!fontsLoaded) {
         return null; // You can return a loading indicator here
     }
+
 
     return (
         <View style={styles.container}>
@@ -41,6 +53,18 @@ export default function HomeScreen({ navigation }) {
                 It's <Text style={styles.name}>73°F</Text> and mostly sunny outside.
             </Text>
 
+            
+            <TestChart />
+            
+            <View style={styles.squares_container}>
+                <Square />
+                <Text style={styles.category}>Plastic</Text>
+                <Square1 />
+                <Text style={styles.category}>Aluminum</Text>
+                <Square2 />
+                <Text style={styles.category}>Paper</Text>
+            </View>
+            
             <View style={styles.summary_container}>
                 <Text style={styles.summary_text}>
                     <Text style={styles.summary_word}>Summary:</Text> 
@@ -99,13 +123,51 @@ const styles = StyleSheet.create({
         color: '#68A77C',
     },
 
+    shapes_container: {
+        backgroundColor: '#0000',
+    },
+
+    squares_container: {
+        flexDirection: 'row', // Arrange squares horizontally
+        alignItems: 'center', // Align squares vertically centered
+        marginVertical: 10, // Add vertical margin
+        marginBottom: 50,
+    },
+
+    square: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#2D5A3D',
+        alignSelf: 'flex-start', // Center the square horizontally
+        marginLeft: 10,
+    },
+
+    square1: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#FFFFFF',
+        alignSelf: 'center', // Center the square horizontally
+        marginLeft: 10,
+    },
+
+    square2: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#FFFFFF',
+        alignSelf: 'center', // Center the square horizontally
+    },
+
+    category: {
+        color: '#000000',
+        marginLeft: 10,
+        alignContent: 'left', 
+    },
+
     summary_container: {
         alignItems: 'center',
         marginBottom: 20,
         width: '100%', // Ensure the container takes the full width
-        justifyContent: 'flex-end',
-        paddingBottom: 30,
-        flex: 1,
+        justifyContent: 'center',
     },
 
     summary_text: {
