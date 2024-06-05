7c34424f6a564b9cea26a5e9094d75b3e5b8c093
kbyung <wkoh@hmc.edu>
2024-06-05 15:19:19 -0700

    firebase config file

---
+++ firebaseConfig.js
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+// firebaseConfig.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore, GeoPoint, Timestamp, collection, addDoc, getDocs } from 'firebase/firestore';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCjLKIUWX2OpKcNjvIUeBSup-mA7IgFjIc",
+  authDomain: "wastewise-f37f3.firebaseapp.com",
+  projectId: "wastewise-f37f3",
+  storageBucket: "wastewise-f37f3.appspot.com",
+  messagingSenderId: "629664245072",
+  appId: "1:629664245072:web:7b1081b88055bad825b855",
+  measurementId: "G-NTM8XBDBT3"
+};
+
+// Initialize Firebase
+// Initialize Firebase
+const FIREBASE_APP = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const FIRESTORE_DB = getFirestore(FIREBASE_APP);
+
+// Initialize Firebase Auth
+const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
+export { FIREBASE_APP, FIRESTORE_DB, GeoPoint, Timestamp, collection, addDoc, getDocs };
